049fa78ef31227aa7e55b000d76b413f2a12261a
VirenPS <viren.samani@hotmail.co.uk>
2021-07-19 14:25:59 +0100

    feature: created class + print function

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from bs4 import BeautifulSoup
 
 
 class Property:
-    def __init__(self, id: int, type: str, address: str, price: str, description: str):
+    def __init__(self, id: int, type: str, address: str, price: str, description: str, branch_summary: str):
         self.id = id
         self.type = type
         self.address = address
-        self.price = address
+        self.price = price
         self.description = description
-        self.property_url: str = 'https://www.rightmove.co.uk/properties/' + \
+        self.branch_summary = branch_summary
+        self.url: str = 'https://www.rightmove.co.uk/properties/' + \
             str(self.id)
 
+    def print_details(self, include_description: bool = False, include_branch_summary: bool = False):
+        print('Type:', self.type)
+        print('Address:', self.address)
+        print('Price:', self.price)
+        if include_description:
+            print('Description:', self.description)
+        if include_branch_summary:
+            print('Description:', self.description)
+        print('URL:', self.url)
 
-def rightmove_URL_generator(location: str, sale_or_rent: str = 'sale', include_description: bool = False):
+
+def generate_rightmove_property_list(location: str, sale_or_rent: str = 'sale'):
     rightmove_URL = 'https://www.rightmove.co.uk/' + \
         f'property-for-{sale_or_rent}' + '/' + location
 
@@ -30,29 +41,32 @@ def rightmove_URL_generator(location: str, sale_or_rent: str = 'sale', include_d
 
     # print('Searching for: ' + soup.find(id='searchTitle').text)
 
-    property_list = []
+    property_list: Property = []
 
     for property_entry in soup.find_all(class_='l-searchResult is-list'):
         html_id = property_entry.get('id')
-        property_id: str = html_id[-html_id.find('-'):]
+        property_id: str = html_id[-(html_id.find('-')+1):]
+        property_list.append(
+            Property(
+                id=property_id,
+                type=property_entry.find(
+                    class_='propertyCard-title').text.strip(),
+                address=property_entry.find(
+                    class_='propertyCard-address').text.strip(),
+                price=property_entry.find(
+                    class_='propertyCard-priceValue').text.strip(),
+                description=property_entry.find(
+                    class_='propertyCard-description').text.strip(),
+                branch_summary=property_entry.find(
+                    class_='propertyCard-branchSummary').text.strip()
 
-        property_type: str = property_entry.find(
-            class_='propertyCard-title').text.strip()
+            ))
 
-        property_address: str = property_entry.find(
-            class_='propertyCard-address').text.strip()
-
-        property_price: str = property_entry.find(
-            class_='propertyCard-priceValue').text.strip()
-
-        property_description: str = property_entry.find(
-            class_='propertyCard-description').text.strip()
-
-        property_list.append(Property(id=property_id, type=property_type, address=property_address,
-                                      price=property_price, description=property_description))
-
-    print(property_list)
+    return property_list
 
 
 if __name__ == '__main__':
-    rightmove_URL_generator(location='Harrow', sale_or_rent='sale')
+    rightmove_property_list = generate_rightmove_property_list(
+        location='Harrow', sale_or_rent='sale')
+    for i in range(0, 5):
+        rightmove_property_list[i].print_details()
